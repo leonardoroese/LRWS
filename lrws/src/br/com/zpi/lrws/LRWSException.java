cba97db7760484fc8f9eb8c2807cb4feed56cdcf
leonardoroese <1qa2ws3ed>
2017-03-10 11:23:23 -0300

    LRWS Exception aditional params

--- lrws/src/br/com/zpi/lrws/LRWSException.java
+++ lrws/src/br/com/zpi/lrws/LRWSException.java
@@ -4,9 +4,27 @@ package br.com.zpi.lrws;
  * Created by leonardo on 19/12/15.
  */
 public class LRWSException extends Exception {
-    private String msg = null;
-    private int errNum = 0;
 
+	private static final long serialVersionUID = 1L;
+	
+	private String msg = null;
+    private int errNum = 0;
+    private String errCode = null;
+    private String errType = null;
+    
+	public LRWSException(){
+    	this.errCode = null;
+    	this.errType = null;
+    	this.msg = null;
+    }
+	
+	public LRWSException(String type, String code, String message){
+    	this.errCode = code;
+    	this.errType = type;
+    	this.msg = message;
+    }
+    
+    
     public int getErrNum() {
 		return errNum;
 	}
@@ -23,4 +41,23 @@ public class LRWSException extends Exception {
     public void setMessage(String message){
         msg = message;
     }
+    
+    public String getErrCode() {
+		return errCode;
+	}
+
+
+	public void setErrCode(String errCode) {
+		this.errCode = errCode;
+	}
+
+
+	public String getErrType() {
+		return errType;
+	}
+
+
+	public void setErrType(String errType) {
+		this.errType = errType;
+	}
 }
